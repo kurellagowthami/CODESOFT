6177a718c82ee2465b49971770c7f616c339ea9d
kurellagowthami <kurellagowthami5@gmail.com>
2025-01-08 20:08:31 +0530

    Add files via upload

---
+++ Rock Paper Scissors-codsoft.py
@@ -0,0 +1,34 @@
+import random
+
+def get_computer_choice():
+    choices = ["rock","paper","scissors"]
+    return random.choice(choices)
+def determine_winner(user_choice,computer_choice):
+    if user_choice == computer_choice:
+        return "its a tie!"
+    elif(
+         (user_choice == "rock" and computer_choice == "scissors") or
+         (user_choice == "paper" and computer_choice == "rock") or
+         (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You Win"
+    else:
+        return "Computer Win"
+def play_game():
+    print("Welcome to Rock Paper Scissors!")
+    while True:
+        user_choice = input("Enter Your choice(rock,paper,scissors) or quit to exit: ").lower()
+        if user_choice == "quit":
+            print("thanks for playing!")
+            break
+        if user_choice not in ["rock","paper","scissors"]:
+            print("invalid choice.please try again")
+            continue
+        computer_choice = get_computer_choice()
+        print(f"you choose :{user_choice}")
+        print(f"computer choose :{computer_choice}")
+        result = determine_winner(user_choice,computer_choice)
+        print(result)
+if __name__ == "__main__":
+    play_game()
+    
