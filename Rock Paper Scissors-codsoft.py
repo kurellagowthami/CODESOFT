8f489c084909aaa5da93d5ae1984460eeadf7a48
kurellagowthami <kurellagowthami5@gmail.com>
2025-01-10 12:22:43 +0530

    Update Rock Paper Scissor-codsoft.py

--- Rock Paper Scissors-codsoft.py
+++ Rock Paper Scissors-codsoft.py
@@ -1,27 +1,27 @@
 import random
 
 def get_computer_choice():
-    choices = ["rock","paper","scissors"]
+    choices = ["rock","paper","scissor"]
     return random.choice(choices)
 def determine_winner(user_choice,computer_choice):
     if user_choice == computer_choice:
         return "its a tie!"
     elif(
-         (user_choice == "rock" and computer_choice == "scissors") or
+         (user_choice == "rock" and computer_choice == "scissor") or
          (user_choice == "paper" and computer_choice == "rock") or
-         (user_choice == "scissors" and computer_choice == "paper")
+         (user_choice == "scissor" and computer_choice == "paper")
     ):
         return "You Win"
     else:
         return "Computer Win"
 def play_game():
-    print("Welcome to Rock Paper Scissors!")
+    print("Welcome to Rock Paper Scissor!")
     while True:
-        user_choice = input("Enter Your choice(rock,paper,scissors) or quit to exit: ").lower()
+        user_choice = input("Enter Your choice(rock,paper,scissor) or quit to exit: ").lower()
         if user_choice == "quit":
             print("thanks for playing!")
             break
-        if user_choice not in ["rock","paper","scissors"]:
+        if user_choice not in ["rock","paper","scissor","quit"]:
             print("invalid choice.please try again")
             continue
         computer_choice = get_computer_choice()
